edff7ca62a750ec0611021fdb472f84762c13f80
nongnapat2017 <nongnapat@gmail.com>
2019-08-15 16:03:00 +0700

    create apps migration

---
+++ database/migrations/2019_08_15_090143_create_apps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apps', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('app_name');
+            $table->string('token');
+            $table->string('secret');
+            $table->json('types');
+            $table->string('size');
+            $table->boolean('year_folder')->default(0);
+            $table->boolean('month_folder')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apps');
+    }
+}
