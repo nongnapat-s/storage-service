dbe164f010e389892f819f7f91431ec75699243e
Koramit Pichanaharee <koramit@users.noreply.github.com>
2020-03-02 21:41:28 +0700

    now we can config local/public root path for disks

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,12 +45,12 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => env('LOCAL_STORAGE_ROOT_PATH', storage_path('app')), //storage_path('app'),
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => env('PUBLIC_STORAGE_ROOT_PATH', storage_path('app')), // storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
